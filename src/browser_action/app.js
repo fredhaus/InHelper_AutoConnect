80b28e130d643dc2399146949a9431caccb62690
Fred Haus <fredhaus@Freds-MacBook-Pro-235.local>
2021-01-08 14:05:28 +0100

    adapting queryselectors to linked in

--- src/browser_action/app.js
+++ src/browser_action/app.js
@@ -1,6 +1,12 @@
+let val = "nix"
+
 document.getElementById('automate-form').onsubmit = event => {
   event.preventDefault()
-  const hashtag = document.getElementById("hashtag").value
-  chrome.tabs.create({url: `https://www.instagram.com/explore/tags/${hashtag}/#automate`})
+  let p = document.createElement("p");
+  p.innerHTML = "TEEEEST"
+  document.body.appendChild(p)
+  chrome.tabs.create({url: `https://www.linkedin.com/in/${val}/#automate`})
 }
+
+
 
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,11 +1,32 @@
 chrome.extension.sendMessage({}, function (response) {
   var readyStateCheckInterval = setInterval(function () {
+    function getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min) + min) * 1000;
+    }
+
+    function sleep(ms) {
+      return new Promise((resolve) => setTimeout(resolve, ms));
+    }
+
+    async function automate() {
+      console.log("Automating!");
+      let timeout1 = getRandomInt(1, 5);
+      let timeout2 = getRandomInt(1, 3);
+      console.log(`sleeping for: ${timeout1}`);
+      await sleep(timeout1);
+      document.querySelector(".pv-s-profile-actions--connect").click();
+      console.log(`sleeping for: ${timeout2}`);
+      await sleep(timeout2);
+      document.querySelector(".ml1").click();
+    }
+
     if (document.readyState === "complete") {
-			clearInterval(readyStateCheckInterval);
-			
-			if (window.location.hash === "#automate"){
-				console.log("Automating!")
-			}
+      clearInterval(readyStateCheckInterval);
+      if (window.location.hash === "#automate") {
+        automate();
+      }
     }
   }, 10);
 });
